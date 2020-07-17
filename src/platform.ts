6472c21fb2632dc6b29cc0fa9891300b9453cfa9
Tom Butcher <thomasbutcher15@gmail.com>
2020-07-17 04:43:53 +0100

    Updated to allow manual configuration in the config.json

--- src/platform.ts
+++ src/platform.ts
@@ -29,42 +29,48 @@ export class MagicHomePlatform implements DynamicPlatformPlugin {
     this.accessories.push(accessory);
   }
 
+  setupDevices(devices) {
+    this.log.info('Found ' + devices.length + ' magichome device(s)');
+    for (const device of devices) {
+
+      const uuid = this.api.hap.uuid.generate(device.id);
+      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      let type;
+      if (device.id in this.config) {
+        type = this.config[device.id];
+      } else {
+        type = 'rgb';
+      }
+      if (existingAccessory) {
+        // the accessory already exists
+        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        new ColorLight(this, existingAccessory, device.address, type);
+
+      } else {
+        // the accessory does not yet exist, so we need to create it
+        this.log.info('Adding new accessory:', device.id + ' : ' + device.model);
+        const accessory = new this.api.platformAccessory(device.id + ' : ' + device.model, uuid);
+        accessory.context.device = device;
+        new ColorLight(this, accessory, device.address, type);
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
+  }
+
   discoverDevices() {
 
     this.log.info('Looking for devices...');
-    const discovery = new Discovery();
-
-
-    discovery.scan(500).then(devices => {
-      
-      // loop over the discovered devices and register each one if it has not already been registered
-      this.log.info('Found ' + devices.length + ' magichome device(s)');
-      for (const device of devices) {
-
-        const uuid = this.api.hap.uuid.generate(device.id);
-        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-        let type;
-        if (device.id in this.config) {
-          type = this.config[device.id];
-        } else {
-          type = 'rgb';
-        }
-        if (existingAccessory) {
-        // the accessory already exists
-          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-          new ColorLight(this, existingAccessory, device.address, type);
-
-        } else {
-        // the accessory does not yet exist, so we need to create it
-          this.log.info('Adding new accessory:', device.id + ' : ' + device.model);
-          const accessory = new this.api.platformAccessory(device.id + ' : ' + device.model, uuid);
-          accessory.context.device = device;
-          new ColorLight(this, accessory, device.address, type);
-          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-        }
-      }
-
-    });
+    if (this.config.discover === false) {
+      this.log.info('Using config.json for device list');
+      this.setupDevices(this.config.devices);
+    } else {
+      this.log.info('Using discovery');
+      const discovery = new Discovery();
+      discovery.scan(500).then(devices => {
+        this.setupDevices(devices);
+      });
+    }
+    
 
   }
 }
